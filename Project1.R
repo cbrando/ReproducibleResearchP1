5f8824f3dbd93573b0d62f37df8d005dea0bb157
cbrando <carlos_brando@yahoo.com>
2015-08-16 17:08:20 -0400

    Completed first part of proj. 1

--- Project1.R
+++ Project1.R
@@ -1,5 +1,6 @@
 #Loading Libraries
 library("dplyr")
+library("markdown")
 # Getting the data
 #Defining URL and file names
 url<-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
@@ -19,14 +20,22 @@ data<-read.csv(filec)
 data<-data[complete.cases(data),]
 
 #Sumarize data per day
-byday <- data %>% group_by(date)
-suma<-byday %>% summarise_each(funs(sum))
+suma<-summarise(group_by(data, date), sum(steps))
 #plot Histogram of total number of step taken each day
 hist(suma$steps)
 
 #Calculate and report the mean and median of the total number of steps taken per day
 mean(suma$steps)
 median(suma$steps)
+
+#average daily activity pattern
+inter<-summarise(group_by(data, interval), mean(steps))
+names(inter)<-c("interval", "stepsmean")
+plot(inter$interval,inter$stepsmean, type="l")
+
+#Interval with maximum of steps
+maxi<-max(inter$stepsmean)
+maxinter<-inter[inter$stepsmean==maxi,1]
 
 #Recalculating with missing values
 #Reading the data
@@ -38,5 +47,5 @@ sum(is.na(data$steps))
 #calculating means per day
 dataf<-data[complete.cases(data),]
 mbyday <- dataf %>% group_by(date)
-mn<-mbyday %>% mutate_each(funs(mean))
+mn<-mbyday %>% summarise_each(funs(mean))
 #####
